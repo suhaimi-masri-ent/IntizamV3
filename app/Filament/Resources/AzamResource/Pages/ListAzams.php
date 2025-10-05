1cef3e57ec59d2e3f501f11cd2e5fd82814ae9a2
suhaimi.masri <suhaimi.masri@gmail.com>
2025-10-06 04:19:13 +0800

    add badge count to the tabs

--- app/Filament/Resources/AzamResource/Pages/ListAzams.php
+++ app/Filament/Resources/AzamResource/Pages/ListAzams.php
@@ -24,15 +24,21 @@ class ListAzams extends ListRecords
     {
         return [
             '<40D' => Tab::make('<40D')
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('duration', '<', 40)),
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('duration', '<', 40))
+                ->badge(fn () => $this->getModel()::where('duration','<', '40')->count()),
             '±40D' => Tab::make('±40D')
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('duration','>', 40)->where('duration','<', 50)),
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('duration','>', 40)->where('duration','<', 50))
+                ->badge(fn () => $this->getModel()::where('duration','>', 40)->where('duration','<', 50)->count()),
             '2B' => Tab::make('2B')
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('duration','=', 60)),
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('duration','=', 60))
+                ->badge(fn () => $this->getModel()::where('duration','=', 60)->count()),
             '4B' => Tab::make('4B')
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('duration','>', 120)->where('duration','<', 360)),
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('duration','>', 120)->where('duration','<', 360))
+                ->badge(fn () => $this->getModel()::where('duration','>', 120)->where('duration','<', 360)->count()),
             '1T' => Tab::make('1T')
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('duration','=', 360)),
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('duration','=', 360))
+                ->badge(fn () => $this->getModel()::where('duration','=', '360')->count()),
+            'All' => Tab::make()->badge(fn () => $this->getModel()::count()),
         ];
     }
 
--- app/Filament/Resources/TafakutResource/Pages/ListTafakuts.php
+++ app/Filament/Resources/TafakutResource/Pages/ListTafakuts.php
@@ -25,23 +25,39 @@ class ListTafakuts extends ListRecords
             '<40D' => Tab::make('<40D')
                 ->modifyQueryUsing(fn (Builder $query) => $query->whereHas('azam', function  (Builder $subQuery) {
                     $subQuery->where('duration','<', '40');
-                })),
+                }))
+                ->badge(fn () => $this->getModel()::whereHas('azam', function  (Builder $subQuery) {
+                    $subQuery->where('duration','<', '40');
+                })->count()),
             '±40D' => Tab::make('±40D')
                 ->modifyQueryUsing(fn (Builder $query) => $query->whereHas('azam', function  (Builder $subQuery) {
                     $subQuery->where('duration','>', '40')->where('duration','<', '60');
-                })),
+                }))
+                ->badge(fn () => $this->getModel()::whereHas('azam', function  (Builder $subQuery) {
+                    $subQuery->where('duration','>', '40')->where('duration','<', '60');
+                })->count()),
             '2B' => Tab::make('2B')
                 ->modifyQueryUsing(fn (Builder $query) => $query->whereHas('azam', function  (Builder $subQuery) {
                     $subQuery->where('duration','=', '60');
-                })),
+                }))
+                ->badge(fn () => $this->getModel()::whereHas('azam', function  (Builder $subQuery) {
+                    $subQuery->where('duration','=', '60');
+                })->count()),
             '4B' => Tab::make('4B')
                 ->modifyQueryUsing(fn (Builder $query) => $query->whereHas('azam', function  (Builder $subQuery) {
                     $subQuery->where('duration','>', '120')->where('duration','<', '360');
-                })),
+                }))
+                ->badge(fn () => $this->getModel()::whereHas('azam', function  (Builder $subQuery) {
+                    $subQuery->where('duration','>', '120')->where('duration','<', '360');
+                })->count()),
             '1T' => Tab::make('1T')
                 ->modifyQueryUsing(fn (Builder $query) => $query->whereHas('azam', function  (Builder $subQuery) {
                     $subQuery->where('duration','=', '360');
-                })),
+                }))
+                ->badge(fn () => $this->getModel()::whereHas('azam', function  (Builder $subQuery) {
+                    $subQuery->where('duration','=', '360');
+                })->count()),
+            'All' => Tab::make()->badge(fn () => $this->getModel()::count()),
         ];
     }
 
